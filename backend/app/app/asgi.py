26bd78ca0f949395eb2063a7d3df760f7a2d0aba
stgerard <stgerard@student.42.fr>
2024-07-08 14:27:27 +0200

    8 07

--- backend/app/app/asgi.py
+++ backend/app/app/asgi.py
@@ -13,7 +13,7 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-# import game.routing
+import game.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
