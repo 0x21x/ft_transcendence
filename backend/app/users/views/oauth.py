1219dc60c3764750a654ea70c41213643a922d1a
stgerard <stgerard@student.42.fr>
2024-07-30 20:41:51 +0200

    30 07

--- backend/app/users/views/oauth.py
+++ backend/app/users/views/oauth.py
@@ -1,11 +1,76 @@
+from django.conf import settings
+from django.contrib.auth.models import User
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .auth import LoginView, RegisterView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.settings import api_settings
+from requests_oauthlib import OAuth2Session
+from rest_framework.permissions import IsAuthenticated
+from ..models import Users
+from ..serlializers import UserSerializer, CustomTokenRefreshSerializer
+from ..tokens import MyTokenViewBase
+from .otp import check_otp
 
-### Ajouter les fonctions pour recuperer les prenoms, noms, emails (enlever le commenteaire une fois fait)
+class OAuthRegisterView(APIView):
+    permission_classes = []
+    
+    def get(self: APIView, request) -> Response:
+        api42 = OAuth2Session(settings.API42_UID, redirect_uri=settings.API42_REDIRECT_URI)
+        url_authorization, state = api42.authorization_url(f"{settings.API42_BASE_URL}/oauth/authorize")
+        
+        request.session['oauth_state'] = state
+        print(f"URL d'autorisation: {url_authorization}, état: {state}")
+        
+        state = request.session.get('oauth_state')
+        print(f"state: {state}")
+        if not state:
+            return Response({"message": "État OAuth manquant. Réessayez la connexion."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        api42 = OAuth2Session(settings.API42_UID, state=state, redirect_uri=settings.API42_REDIRECT_URI)
+        print(f"api42: {api42}")
+        print(f"settings.API42_UID: {settings.API42_UID}")
+        print(f"state: {state}")
+        print(f"settings.API42_REDIRECT_URI: {settings.API42_REDIRECT_URI}")
+        print("before token")
+        try:
+            token = api42.fetch_token(
+				f"{settings.API42_BASE_URL}/oauth/token",
+				client_secret=settings.API42_SECRET,
+				authorization_response=request.get_full_path()
+			)
+            print(f"Token: {token}")
+        except Exception as e:
+            print(f"Erreur lors de la récupération du token: {e}")
+            return Response({"message": "Erreur lors de la récupération du token"}, status=status.HTTP_400_BAD_REQUEST)
 
-class OAuthRegisterView(RegisterView):
-    def post(self: APIView, request: any) -> Response:
+        try:
+            user_info = api42.get(f"{settings.API42_BASE_URL}/v2/me").json()
+        except Exception as e:
+            print(f"Erreur lors de la récupération des informations utilisateur: {e}")
+            return Response({"message": "Erreur lors de la récupération des informations utilisateur"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        prenom = user_info.get('first_name')
+        nom = user_info.get('last_name')
+        email = user_info.get('email')
+        username = user_info.get('login')
+        print(f"Prénom: {prenom}, Nom: {nom}, Email: {email}, Nom d'utilisateur: {username}")
+        
+        if Users.objects.filter(username=request.data["username"]).exists():
+            return Response({"Username already exists"}, status=status.HTTP_409_CONFLICT)
+        
+        rdata = {'username': username, 'password': '1234'}
+        user = UserSerializer(data=rdata)
+        if not user.is_valid():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        user.save()
+        return Response(user.data, status=status.HTTP_200_OK)
+
+
+# class OAuthRegisterView(APIView):
+#     def post(self: APIView, request: any) -> Response:
         """
             :param request: Any
             :return A Response with the user.data
@@ -16,7 +81,7 @@ class OAuthRegisterView(RegisterView):
         """
         pass
 
-class OAuthLoginView(LoginView):
+class OAuthLoginView(APIView):
     def post(self: APIView, request: any) -> Response:
         """
             :param request: Any
