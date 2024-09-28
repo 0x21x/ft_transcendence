057984969ad89edb7965764155262dba80ac9a05
stgerard <stgerard@student.42.fr>
2024-09-28 14:31:44 +0200

    28 09

--- backend/app/users/views/oauth.py
+++ backend/app/users/views/oauth.py
@@ -15,11 +15,11 @@ from ..tokens import MyTokenViewBase
 from .otp import check_otp
 from .auth import LoginView, RegisterView
 
-### Ajouter les fonctions pour recuperer les prenoms, noms, emails (enlever le commenteaire une fois fait)
-
 User = get_user_model()
 
 class OAuthCallbackView(APIView):
+    permission_classes = []
+    
     def get(self, request) -> Response:
         
         state = request.data.get('code')
@@ -39,6 +39,37 @@ class OAuthCallbackView(APIView):
                 print(f"Erreur lors de la récupération du token : {str(e)}")
                 return Response({"message": f"Erreur lors de la récupération du token: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
         
+        user_info = api42.get(f"{settings.API42_BASE_URL}/v2/me").json()
+        print(f"Informations utilisateur récupérées : {user_info}")
         
-         
-        return Response({"token": token}, status=status.HTTP_200_OK)
+        prenom = user_info.get('first_name')
+        nom = user_info.get('last_name')
+        email = user_info.get('email')
+        username = user_info.get('login')
+        print(f"Prénom : {prenom}, Nom : {nom}, Email : {email}, Nom d'utilisateur : {username}")
+        
+        if User.objects.filter(username=username).exists():
+            print(f"Nom d'utilisateur {username} déjà existant")
+            return Response({"message": "Nom d'utilisateur déjà existant"}, status=status.HTTP_409_CONFLICT)
+        
+        print(f"Création de l'utilisateur avec le nom d'utilisateur {username}")
+        user_data = {'username': username, 'password': User.objects.make_random_password()}
+        user_serializer = UserSerializer(data=user_data)
+        if not user_serializer.is_valid():
+            print(f"Erreurs de validation lors de la création de l'utilisateur : {user_serializer.errors}")
+            return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        user_serializer.save()
+        print(f"Utilisateur {username} créé avec succès")
+        
+        user = User.objects.get(username=username)
+        refresh = RefreshToken.for_user(user)
+        
+        print(f"Tokens JWT générés pour l'utilisateur {username} : refresh={str(refresh)}, access={str(refresh.access_token)}")
+
+        return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }, status=status.HTTP_201_CREATED)
+        
+        
